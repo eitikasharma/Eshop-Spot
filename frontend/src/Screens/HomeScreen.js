35f04eeeb9bab1f7b45740666ec666b1554ec450
eitikasharma <eitikasharma1999@gmail.com>
2020-11-30 21:41:33 +0530

    ProductScreen completed

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -9,15 +9,16 @@ function HomeScreen(props)
       data.products.map(product =>
         <li key={product._id}>
         <div className="product" >
-
          
-          <img className="product-image" src={product.image} alt="product_image" ></img>
           <div className="product-name">
-          <Link to={'/product/'+product._id}>{product.name} 
+          <Link to={'/product/'+product._id}>
+          <img className="product-image" src={product.image} alt="product_image" ></img>
+            {product.name} 
           </Link></div>
           <div className="product-brand">{product.brand}</div>
           <div className="product-price">Rs{product.price}</div>
           <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
+          
         </div>
       </li>
         )
