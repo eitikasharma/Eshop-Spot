eb3311fa136f78fcec9b50a1fdbc4ccc049e49b8
eitikasharma <eitikasharma1999@gmail.com>
2020-12-01 16:26:43 +0530

    Created Node Server

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+  products:[
+      {
+        _id:'1',
+      name:'Slim Shirt',
+      category:'Shirts',
+      image:'/images/d1.jpg',
+      price:1399,
+      brand:'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id:'2',
+    name:'Fit Shirt',
+    category:'Shirts',
+    image:'/images/d1.jpg',
+    price:1699,
+    brand:'Nike',
+    rating: 4.0,
+    numReviews: 7
+    },
+    {
+      _id:'3',
+    name:'Best Pants',
+    category:'Pants',
+    image:'/images/d1.jpg',
+    price:1299,
+    brand:'Nike',
+    rating: 4.5,
+    numReviews: 8
+    }
+]
+}
